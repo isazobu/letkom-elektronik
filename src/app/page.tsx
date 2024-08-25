9b3182a00a2cb5181f95e273b06f96b339287e40
İsa Zobu <isa.zobu@getmidas.com>
2024-08-25 03:23:40 +0300

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
       <Hero />
       <Features />
       <Brands />
+      <Contact />
       <AboutSectionOne />
       <AboutSectionTwo />
       <Testimonials />
